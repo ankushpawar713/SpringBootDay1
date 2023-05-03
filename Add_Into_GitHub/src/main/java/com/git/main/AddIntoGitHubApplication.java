ffae3e65c8cf840b0a08052905207aab8cf824be
ankushpawar713 <ankushpawar713@gmail.com>
2023-05-03 11:47:08 +0530

    first commit

---
+++ Add_Into_GitHub/src/main/java/com/git/main/AddIntoGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.git.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddIntoGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddIntoGitHubApplication.class, args);
+	}
+
+}
---
+++ Add_Into_GitHub/src/test/java/com/git/main/AddIntoGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddIntoGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
